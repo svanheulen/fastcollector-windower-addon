9037b5058ca7b7069572e89fa183f7ad3683aa74
svanheulen <svanheulen@gmail.com>
2015-01-20 14:41:03 -0500

    fixed typo in set creation

--- fastcollector.lua
+++ fastcollector.lua
@@ -170,7 +170,7 @@ function fastcollector_command(...)
             local new_set = {}
             ignore = S(settings.ignore)
             for _,item in pairs(windower.ffxi.get_items().inventory) do
-                if type(inv_item) == 'table' and item.id ~= 0  and not ignore:contains(res.items[item.id].name) then
+                if type(item) == 'table' and item.id ~= 0  and not ignore:contains(res.items[item.id].name) then
                     table.insert(new_set, res.items[item.id].name)
                 end
             end
